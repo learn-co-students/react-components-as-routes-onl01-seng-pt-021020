226aa9f2344da924a5ef03e9b7c39bd6148145cf
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-10-27 15:29:55 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
-
-const About = () => {
-  return (
-    <div>
-      <h1>About!</h1>
-    </div>
-  )
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
 }
 
-const Login = () => {
-  return (
-    <div>
+const Navbar = () =>
+  <div>
+    <NavLink 
+      to="/" 
+      exact 
+      style={link} 
+      activeStyle={{background: "darkblue"}}
+    >About</NavLink>
+    <NavLink 
+      to="/login" 
+      exact
+      style={link}
+      activeStyle={{background: "darkblue"}}
+      >Login</NavLink>
+  </div>
+
+const Home = () => <h1>Home Component</h1>
+
+const About = () => <h1>About Component</h1>
+
+const Login = () => 
       <form>
+        <h1>Login</h1>
         <div>
           <input type="text" name="username" placeholder="Username" />
           <label htmlFor="username">Username</label>
@@ -32,14 +44,12 @@ const Login = () => {
         </div>
         <input type="submit" value="Login" />
       </form>
-    </div>
-  );
-};
 
 ReactDOM.render(
   <Router>
     <div>
-      <Route path="/" component={Home} />
+      <Navbar />
+      <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/login" component={Login} />
     </div>
